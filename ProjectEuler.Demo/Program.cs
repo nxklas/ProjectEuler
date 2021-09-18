e6344d6736477a5dd69ce90dcd61db684ab3baf7
nxklas <niklas-bayer@gmx.de>
2021-09-18 16:32:28 +0200

    Initialized project

---
+++ ProjectEuler.Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ProjectEuler.Demo
+{
+    /// <summary>
+    /// Provides the main methods for this application
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        private static void Main()
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
