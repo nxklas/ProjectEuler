67e4692b0da95d62bec056371b0f1d37bebe9e5b
nxklas <niklas-bayer@gmx.de>
2021-09-18 19:03:05 +0200

    Optimized algorithm in Problem1

--- ProjectEuler/Solutions/Problem1.cs
+++ ProjectEuler/Solutions/Problem1.cs
@@ -38,7 +38,7 @@ namespace ProjectEuler.Solutions
         {
             int result = 0;
 
-            for (int i = 0; i < 1000; i++)
+            for (int i = 1; i < 1000; i++)
                 if (i % 3 == 0 || i % 5 == 0)
                     result += i;
             return result.ToString();
